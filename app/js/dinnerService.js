69f57934b7b60e37410a27d7abe42a74232b1b5c
jacob <jacob.norlinandersson@gmail.com>
2015-06-09 18:09:21 +0200

    Updated search function to use rxjs

--- app/js/dinnerService.js
+++ app/js/dinnerService.js
@@ -161,7 +161,7 @@ dinnerPlannerApp.factory('Dinner',function ($resource, $cookieStore) {
 
   var apiKey = "dvxzUl57TpT2oAV9h288wTuTCEEQE8z8";
 
-  this.DishSearch = $resource('http://api.bigoven.com/recipes',{pg:1,rpp:5,api_key:apiKey});
+
   this.Dish = $resource('http://api.bigoven.com/recipe/:id',{api_key:apiKey}); 
 
   deCookiefySelectedDishes($cookieStore.get('selectedDishes'));
@@ -171,21 +171,6 @@ dinnerPlannerApp.factory('Dinner',function ($resource, $cookieStore) {
 
 
 
-
-  // TODO in Lab 5: Add your model code from previous labs
-  // feel free to remove above example code
-  // you will need to modify the model (getDish and getAllDishes) 
-  // a bit to take the advantage of Angular resource service
-  // check lab 5 instructions for details
-
-
-
-
-
-  // Angular service needs to return an object that has all the
-  // methods created in it. You can consider that this is instead
-  // of calling var model = new DinnerModel() we did in the previous labs
-  // This is because Angular takes care of creating it when needed.
   return this;
 
 });
--- app/js/searchCtrl.js
+++ app/js/searchCtrl.js
@@ -1,26 +1,50 @@
 // Search controller that we use whenever we have a search inputs
 // and search results
-dinnerPlannerApp.controller('SearchCtrl', function ($scope,$routeParams, Dinner) {
+dinnerPlannerApp.controller('SearchCtrl', function ($scope,$routeParams, $http, Dinner) {
 
-  // TODO in Lab 5: you will need to implement a method that searchers for dishes
-  // including the case while the search is still running.
 
   	$scope.query = $routeParams.searchTerm;
 
-  	
-
   	$scope.search = function(query) {
-  		console.debug(query)
+		var apiKey = "dvxzUl57TpT2oAV9h288wTuTCEEQE8z8";
+
   		Dinner.setLastSearchTerm(query);
   		$scope.query = Dinner.getLastSearchTerm();
 		$scope.status = "Searching...";
-		Dinner.DishSearch.get({title_kw:query},
+		var showMoreClickStream = Rx.Observable.fromEvent($('#showMoreButton'), 'click')
+
+		var pg = 1
+		
+		//Map every click in the stream to a new search for a new page
+		var searchStream = showMoreClickStream.flatMap(
+			function(){
+				pg++;
+				return Rx.Observable.fromPromise(
+						$http(
+		    				{method: 'GET',
+		    		        url: 'http://api.bigoven.com/recipes',
+		    		        params: { api_key:apiKey, pg:pg, rpp:20, title_kw:query}}
+						)
+					);	
+			}
+		).merge(Rx.Observable.fromPromise(
+					$http(
+		    				{method: 'GET',
+		    		        url: 'http://api.bigoven.com/recipes',
+		    		        params: { api_key:apiKey, pg:1, rpp:20, title_kw:query}}
+						)
+					)
+		)
+
+		//Add new dishes to page
+		searchStream.subscribe(
 			function(data){
-				$scope.dishes = data.Results;
-				$scope.status = "Showing " + data.Results.length + " results";
+				$scope.dishes = _.union($scope.dishes, data.data.Results)
+				$scope.status = ""
 			},
 			function(data){
-				$scope.status = "There was an error";
+				searchStream = searchStream.skip(1);
+				$scope.status = "There was an error, please try again";
 			}
 		);
 	}
